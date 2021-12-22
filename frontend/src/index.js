9942e91162f672b1ce0a38c037eb7c026bfeee06
tommivk <tommikokk@gmail.com>
2021-12-22 22:42:56 +0200

    add temporary account creation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,135 +1,26 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-import { SigningCosmWasmClient } from "secretjs";
-
+import setupKeplr from "./setupKeplr";
+import getNewAccount from "./newAccount";
 const CHAIN_ID = "secretdev-1";
-const contractAddress = "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg";
+const REST_URL = "http://localhost:1337";
+// const contractAddress = "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg";
+const contractAddress = "secret1hqrdl6wstt8qzshwc6mrumpjk9338k0lpsefm3";
 
 const App = () => {
   const [account, setAccount] = useState(null);
-  const [keplrReady, setKeplrReady] = useState(false);
   const [signingClient, setSigningClient] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      await setupKeplr();
-      console.log("done");
-    })();
-  }, []);
-
-  const setupKeplr = async () => {
-    // Define sleep
-    const sleep = (ms) => new Promise((accept) => setTimeout(accept, ms));
-
-    // Wait for Keplr to be injected to the page
-    while (
-      !window.keplr &&
-      !window.getOfflineSigner &&
-      !window.getEnigmaUtils
-    ) {
-      await sleep(10);
-    }
-    await sleep(2000);
-
-    // Use a custom chain with Keplr.
-    // On mainnet we don't need this (`experimentalSuggestChain`).
-    // This works well with `enigmampc/secret-network-sw-dev`:
-    //     - https://hub.docker.com/r/enigmampc/secret-network-sw-dev
-    //     - Run a local chain: `docker run -it --rm -p 26657:26657 -p 26656:26656 -p 1337:1337 -v $(shell pwd):/root/code --name secretdev enigmampc/secret-network-sw-dev`
-    //     - `alias secretcli='docker exec -it secretdev secretcli'`
-    //     - Store a contract: `docker exec -it secretdev secretcli tx compute store /root/code/contract.wasm.gz --from a --gas 10000000 -b block -y`
-    // On holodeck, set:
-    //     1. CHIAN_ID = "holodeck-2"
-    //     2. rpc = "ttp://chainofsecrets.secrettestnet.io:26657"
-    //     3. rest = "https://chainofsecrets.secrettestnet.io"
-    //     4. chainName = Whatever you like
-    // For more examples, go to: https://github.com/chainapsis/keplr-example/blob/master/src/main.js
-    await window.keplr.experimentalSuggestChain({
-      chainId: CHAIN_ID,
-      chainName: "Local Secret Chain",
-      rpc: "http://localhost:26657",
-      rest: "http://localhost:1337",
-      bip44: {
-        coinType: 529,
-      },
-      coinType: 529,
-      stakeCurrency: {
-        coinDenom: "SCRT",
-        coinMinimalDenom: "uscrt",
-        coinDecimals: 6,
-      },
-      bech32Config: {
-        bech32PrefixAccAddr: "secret",
-        bech32PrefixAccPub: "secretpub",
-        bech32PrefixValAddr: "secretvaloper",
-        bech32PrefixValPub: "secretvaloperpub",
-        bech32PrefixConsAddr: "secretvalcons",
-        bech32PrefixConsPub: "secretvalconspub",
-      },
-      currencies: [
-        {
-          coinDenom: "SCRT",
-          coinMinimalDenom: "uscrt",
-          coinDecimals: 6,
-        },
-      ],
-      feeCurrencies: [
-        {
-          coinDenom: "SCRT",
-          coinMinimalDenom: "uscrt",
-          coinDecimals: 6,
-        },
-      ],
-      gasPriceStep: {
-        low: 0.1,
-        average: 0.25,
-        high: 0.4,
-      },
-      features: ["secretwasm"],
-    });
-
-    // Enable Keplr.
-    // This pops-up a window for the user to allow keplr access to the webpage.
-    await window.keplr.enable(CHAIN_ID);
-
-    // Setup SecrtJS with Keplr's OfflineSigner
-    // This pops-up a window for the user to sign on each tx we sent
-    const keplrOfflineSigner = window.getOfflineSigner(CHAIN_ID);
-    const accounts = await keplrOfflineSigner.getAccounts();
-    console.log("accounts", accounts);
-
-    const signingClient = new SigningCosmWasmClient(
-      "http://localhost:1337", // holodeck - https://chainofsecrets.secrettestnet.io; mainnet - user your LCD/REST provider
-      accounts[0].address,
-      keplrOfflineSigner,
-      window.getEnigmaUtils(CHAIN_ID),
-      {
-        // 300k - Max gas units we're willing to use for init
-        init: {
-          amount: [{ amount: "300000", denom: "uscrt" }],
-          gas: "300000",
-        },
-        // 300k - Max gas units we're willing to use for exec
-        exec: {
-          amount: [{ amount: "300000", denom: "uscrt" }],
-          gas: "300000",
-        },
-      }
-    );
-
-    setSigningClient(signingClient);
-    setAccount(accounts[0]);
-    setKeplrReady(true);
-  };
 
   const queryBoard = async () => {
     try {
-      const res = await signingClient?.queryContractSmart(contractAddress, {
-        get_board: {},
-      });
-      console.log(res);
+      const response = await signingClient?.queryContractSmart(
+        contractAddress,
+        {
+          get_board: {},
+        }
+      );
+      console.log(response);
     } catch (err) {
       console.log(err);
     }
@@ -137,25 +28,61 @@ const App = () => {
 
   const join = async () => {
     try {
-      const res = await signingClient?.execute(contractAddress, {
+      const response = await signingClient?.execute(contractAddress, {
         join: {},
       });
-      console.log(res);
+      console.log("res", response);
     } catch (err) {
       console.log(err);
     }
   };
+  const [selectedNumber, setSelectedNumber] = useState(null);
 
-  return keplrReady ? (
+  const quess = async () => {
+    try {
+      const result = await signingClient?.execute(contractAddress, {
+        quess: { index: Number(selectedNumber) },
+      });
+      console.log("idn");
+      console.log("aaa", { result });
+    } catch (error) {
+      console.log(error?.message);
+    }
+  };
+
+  const connectKeplr = async () => {
+    await setupKeplr(CHAIN_ID, setAccount, setSigningClient);
+  };
+
+  const createAccount = async () => {
+    const mnemonic = localStorage.getItem("secretmines");
+    let account = await getNewAccount(
+      REST_URL,
+      setSigningClient,
+      setAccount,
+      mnemonic
+    );
+    localStorage.setItem("secretmines", JSON.stringify(account.mnemonic));
+  };
+
+  return signingClient ? (
     <div>
-      <p>asdsa {account?.address}</p>
+      <p>Your account: {account?.address}</p>
       <div>
         <button onClick={() => queryBoard()}>Query board</button>
         <button onClick={() => join()}>Join</button>
+        <input
+          type="number"
+          onChange={({ target }) => setSelectedNumber(target.value)}
+        ></input>
+        <button onClick={() => quess()}>Quess</button>
       </div>
     </div>
   ) : (
-    <div>loading....</div>
+    <div>
+      <button onClick={() => connectKeplr()}>Connect keplr wallet</button>
+      <button onClick={() => createAccount()}>Create temporary account</button>
+    </div>
   );
 };
 
---
+++ frontend/src/newAccount.js
@@ -0,0 +1,73 @@
+import {
+  CosmWasmClient,
+  Secp256k1Pen,
+  pubkeyToAddress,
+  encodeSecp256k1Pubkey,
+  EnigmaUtils,
+  SigningCosmWasmClient,
+} from "secretjs";
+
+import { Bip39, Random } from "@iov/crypto";
+
+const getNewAccount = async (
+  REST_URL,
+  setSigningClient,
+  setAccount,
+  usermnemonic
+) => {
+  //Use mnemonic from localstorage if it exists or create new mnemonic
+  const trimmedMnemonic = usermnemonic?.replace(/"/g, "");
+  const mnemonic =
+    trimmedMnemonic ?? Bip39.encode(Random.getBytes(16)).toString();
+
+  // This wraps a single keypair and allows for signing.
+  const signingPen = await Secp256k1Pen.fromMnemonic(mnemonic);
+
+  // Get the public key
+  const pubkey = encodeSecp256k1Pubkey(signingPen.pubkey);
+
+  // Get the wallet address
+  const accAddress = pubkeyToAddress(pubkey, "secret");
+
+  // Query the account
+  const client = new CosmWasmClient(REST_URL);
+  const account = await client.getAccount(accAddress);
+
+  console.log("mnemonic: ", mnemonic);
+  console.log("address: ", accAddress);
+  console.log("account: ", account);
+  console.log(accAddress);
+
+  const customFees = {
+    upload: {
+      amount: [{ amount: "2000000", denom: "uscrt" }],
+      gas: "2000000",
+    },
+    init: {
+      amount: [{ amount: "500000", denom: "uscrt" }],
+      gas: "500000",
+    },
+    exec: {
+      amount: [{ amount: "500000", denom: "uscrt" }],
+      gas: "500000",
+    },
+    send: {
+      amount: [{ amount: "80000", denom: "uscrt" }],
+      gas: "80000",
+    },
+  };
+
+  const txEncryptionSeed = EnigmaUtils.GenerateNewSeed();
+  const signingClient = new SigningCosmWasmClient(
+    REST_URL,
+    accAddress,
+    (signBytes) => signingPen.sign(signBytes),
+    txEncryptionSeed,
+    customFees
+  );
+  setSigningClient(signingClient);
+  setAccount(account);
+  return { mnemonic, accAddress };
+};
+
+export default getNewAccount;
