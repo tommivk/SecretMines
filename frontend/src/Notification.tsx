5873e0d07d0aa5e0bb71ec3606bccd79854fa9c3
tommivk <tommikokk@gmail.com>
2022-01-28 17:40:43 +0200

    convert Notification to typescript

--- frontend/src/Notification.tsx
+++ frontend/src/Notification.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 
-const Notification = ({ notificationData, setShowNotification }) => {
+type Props = {
+  notificationData: { text: string; type: string };
+  setShowNotification: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Notification = ({ notificationData, setShowNotification }: Props) => {
   return (
     <div
       className={`notification  ${
