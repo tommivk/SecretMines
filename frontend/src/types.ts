ad18941cb34bef12f43252ea16ac802caf10e227
tommivk <tommikokk@gmail.com>
2022-01-28 21:23:19 +0200

    add UserAccount type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,15 +1,24 @@
+import { Account } from "secretjs";
+
 export type GameInfo = {
-        address: string;
-        label: string;
-}
+  address: string;
+  label: string;
+};
 
 export type GameState = {
-        board: Array<number>,
-        game_over: boolean,
-        winner: string | undefined,
-        player_a: string | undefined,
-        player_b: string | undefined,
-        player_a_wants_rematch: boolean,
-        player_b_wants_rematch: boolean,
-        turn: string | undefined,
-}
+  board: Array<number>;
+  game_over: boolean;
+  winner: string | undefined;
+  player_a: string | undefined;
+  player_b: string | undefined;
+  player_a_wants_rematch: boolean;
+  player_b_wants_rematch: boolean;
+  turn: string | undefined;
+};
+
+export type NewAccount = {
+  address: string;
+  balance: undefined;
+};
+
+export type UserAccount = Account | NewAccount;
