c92262b2dc446c50df9dfb9b4809dd682541b5aa
tommivk <tommikokk@gmail.com>
2022-01-28 17:58:41 +0200

    add GameState type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -2,3 +2,14 @@ export type GameInfo = {
         address: string;
         label: string;
 }
+
+export type GameState = {
+        board: Array<number>,
+        game_over: boolean,
+        winner: string | undefined,
+        player_a: string | undefined,
+        player_b: string | undefined,
+        player_a_wants_rematch: boolean,
+        player_b_wants_rematch: boolean,
+        turn: string | undefined,
+}
